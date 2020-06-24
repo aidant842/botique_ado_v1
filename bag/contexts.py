437f3b281cd96f05e515e7421965780ab4d55351
aidant842 <aidant842@gmail.com>
2020-06-24 08:45:17 +0000

    error with bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,3 @@
-""" Decimal is being used here instead of float because we are dealing with currency,
-    and float is susceptible to rounding errors, therefore decimal is more accurate """
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
@@ -13,25 +11,37 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
--- bag/views.py
+++ bag/views.py
@@ -10,7 +10,7 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """ Add specified quantity of product to shopping bag """
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
