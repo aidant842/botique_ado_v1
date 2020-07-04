665e27cc591a59cb68857fe44d8a16ecb35996f4
aidant842 <aidant842@gmail.com>
2020-07-04 09:00:58 +0000

    Product Admin - add product view url and template

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    # specify that product id must be an int, otherwise
+    # navigating to products/add will interperate the
+    # string as an id and throw an error
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
