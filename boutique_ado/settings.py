a9ec151ba0ecbee98ece9bdd6d1a70ce7c043d98
aidant842 <aidant842@gmail.com>
2020-07-07 11:50:22 +0000

    Added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -179,6 +179,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'aidant842-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-west-1'
